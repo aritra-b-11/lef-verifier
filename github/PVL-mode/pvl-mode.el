4be32e9734d5e326d8d9c7f5885a9b9d4e912bf9
Aritra-b-11 <aritrabhattacharjee12@gmail.com>
2017-05-20 11:15:48 +0530

    started

---
+++ github/PVL-mode/pvl-mode.el
@@ -0,0 +1,19 @@
+;;; package --- Summary
+
+;; Mode Name: PVL Mode
+;; Author: Aritra Bhattacharjee
+;; Email: aritrabhattacharjee12@gmail.com
+;; version: 0.1
+;; Release Date:TBD
+
+;;; Commentary:
+
+;; PVL mode is a mode for writing the PVS Rule deck.
+
+;;; Code:
+(defvar pvl-mode-hook nil "This is the PVL mode for wiriting the PVS Rule Reck.")
+
+
+
+(provide 'pvl-mode)
+;;; pvl-mode.el ends here
