caf7c16aae9e8afabd4ae68fa7fce6dd5fe27cd3
Aritra-b-11 <aritrabhattacharjee12@gmail.com>
2017-05-25 07:23:10 +0530

    added more fun

--- github/lef-verifier/lef-verifier.el
+++ github/lef-verifier/lef-verifier.el
@@ -1,6 +1,18 @@
 ;;; package -- summary
 
+;; Package Name: lef verifier
+;; Author: Aritra Bhattacharjee
+;; Email: aritrabhattacharjee12@gmail.com
+;; version: 0.1
+;; Release Date: TBD
+
 ;;; Commentary:
+
+;; This is a collection of some useful function needed for lef verification.
+;; The functions are :
+;; add-class-block () for adding class block after macro
+;; replace-ground-use () for replacing the use signal for ground pins
+;; replace-power-use () for replacing the use signal for power pins
 
 ;;; Code:
 (defun add-class-block ()
@@ -9,8 +21,8 @@
   (goto-char (point-min))
   (search-forward-regexp "^[ ]*\\(macro\\|MACRO\\)[ ]+[^ ]")
   (end-of-line)
-  (insert "\nCLASS BLOCK ;")
-  )
+  (insert "\nCLASS BLOCK ;"))
+
 
 (defvar default-timeout 5 "Default timeout value for y-or-n.")
 
@@ -18,14 +30,24 @@
   "Replace ground from use signal to use ground."
   (interactive)
   (goto-char (point-min))
-  (search-forward-regexp "^[ ]*\\(pin\\|PIN\\)[ ]+\\(agnd\\|AGND\\|gnd\\|GND\\|VSS\\|AVSS\\)")
-  (search-forward-regexp "^[ ]*USE[ ]+\\([[:alpha:]]+\\)[ ]*$")
-  (if (equal (match-string 1) "signal")
-      (if (y-or-n-p-with-timeout "Replace with Ground? " default-timeout "y")
-	  (progn (kill-word -1)
-		 (insert "GROUND"))
-	))
-  )
+  (while (search-forward-regexp "^[ ]*\\(pin\\|PIN\\)[ ]+[^ ]*\\(gnd\\|GND\\|VSS\\|vss\\)" nil t)
+    (search-forward-regexp "^[ ]*USE[ ]+\\([[:alpha:]]+\\)[ ]*$" nil t)
+    (if (equal (match-string 1) "signal")
+	(if (y-or-n-p-with-timeout "Replace with Ground? " default-timeout "y")
+	    (progn (kill-word -1)
+		   (insert "GROUND"))))))
+
+(defun replace-power-use ()
+  "Replace ground from use signal to use ground."
+  (interactive)
+  (goto-char (point-min))
+  (while (search-forward-regexp "^[ ]*\\(pin\\|PIN\\)[ ]+[^ ]*\\(vdd\\|VDD\\)" nil t)
+    (search-forward-regexp "^[ ]*USE[ ]+\\([[:alpha:]]+\\)[ ]*$" nil t)
+    (if (equal (match-string 1) "signal")
+	(if (y-or-n-p-with-timeout "Replace with POWER? " default-timeout "y")
+	    (progn (kill-word -1)
+		   (insert "POWER"))))))
+
 
 ;;; lef-verifier.el ends here
 
